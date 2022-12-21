f41b2bcbd6ef3c807da0d66612c27cbc79e60d6b
Yurun <admin@yurunsoft.com>
2022-12-21 11:28:16 +0800

    支持自动编译发布

--- build.rs
+++ build.rs
@@ -1,11 +1,16 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::process::exit;
 
 fn main() {
-    let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let output_path = Path::new(&manifest_dir_string).join("target").join(env::var("PROFILE").unwrap());
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("manifest_dir={}", manifest_dir);
+    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("out_dir={}", out_dir);
+    let output_path = Path::new(&out_dir).parent().unwrap().parent().unwrap().parent().unwrap();
+    println!("output_path={}", output_path.to_str().unwrap().to_string());
 
     println!("Copy config.json...");
-    fs::copy(Path::new(&manifest_dir_string).join("config.json"), Path::new(&output_path).join("config.json")).unwrap();
+    fs::copy(Path::new(&manifest_dir).join("config.json"), output_path.join("config.json")).unwrap();
 }
